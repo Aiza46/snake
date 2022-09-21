3f45f2c3bb35b871f2d6a02c284c8834ddb26cad
Aiza <aizakona.j@gmail.com>
2022-09-21 15:05:41 +0600

    create class

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -1,14 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-int x1 = 1;
-int y1 = 3;
-char sym1 = '#';
-Draw(x1, y1, sym1);
-int x2 = 4;
-int y2 = 5;
-char sym2 = '*';
-Draw(x2, y2, sym2);
+﻿// See https://aka.ms/new-console-template for more informatio
+
+using System.Drawing;
+
+Point p1 = new Point();
+p1.X = 1;
+p1.Y = 3;
+p1.sym = '*';
+p1.Draw();
+
+Point p2 = new Point();
+p2.X = 4;
+p2.Y = 5;
+p2.sym = '*';
+p2.Draw();
 Console.ReadLine();
-static void Draw(int x, int y, char sym)
-{   Console.SetCursorPosition(x,y);
-    Console.Write(sym);}
-  
